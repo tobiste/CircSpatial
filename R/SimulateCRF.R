0774d28a3784608cde97fa27493a6542ddf52804
Tobias Stephan <73840881+tobiste@users.noreply.github.com>
2024-05-16 12:57:22 -0400

    random variables generation in separate functions

--- R/SimulateCRF.R
+++ R/SimulateCRF.R
@@ -1,3 +1,84 @@
+CDF2dir <- function(CircProb, CumProbZ, N, CircScale){
+  n <- length(CircScale)
+  CircProb[1] <- 0
+  CircProb[n] <- 1
+
+  # Interpolation
+  DeltaTh <- CircScale[2] + pi
+  direction <- vector(mode = "numeric", length = N)
+
+  for (i in 1:N)
+  {
+    p <- CumProbZ[i] # Cumulative prob of GRV
+    a <- max((1:n)[CircProb <= p]) # Index
+    if (a == n) {
+      r <- 0
+    } else {
+      if (CircProb[a] == p) {
+        r <- 0
+      } else {
+        r <- (p - CircProb[a]) / (CircProb[a + 1] - CircProb[a])
+      }
+    }
+    direction[i] <- CircScale[a] + r * DeltaTh
+  }
+return(direction)
+}
+
+CDF_vM <- function(Rho, CircScale){
+  stopifnot(!(Rho == 0 | Rho >= 1))
+  n <- length(CircScale)
+  # if (Rho == 0 | Rho >= 1) stop("vM: 0 < Rho < 1")
+  CircProb <- rep(-1, n)
+  Kappa <- CircStats::A1inv(Rho) # N. I Fisher, Statistical Analysis of Circular Data, 2000 p. 49
+  # As direction increases from -pi, pvm increases from .5
+
+  for (i in 1:length(CircScale)) CircProb[i] <- CircStats::pvm(CircScale[i], mu = 0, kappa = Kappa)
+  filter <- CircScale < 0
+  CircProb[filter] <- CircProb[filter] - 0.5
+  CircProb[!filter] <- CircProb[!filter] + 0.5
+  return(CircProb)
+}
+
+CDF_WrC <- function(Rho, CircScale){
+  stopifnot(!(Rho == 0 | Rho >= 1))
+  # if (Rho == 0 | Rho >= 1) stop("Wrapped Cauchy: 0 < Rho < 1 ")
+  Angles1 <- CircScale[CircScale < 0]
+  Angles2 <- CircScale[CircScale >= 0]
+  prob1 <- 0.5 - acos(((1 + Rho^2) * cos(Angles1) - 2 * Rho) / (1 + Rho^2 - 2 * Rho * cos(Angles1))) / (2 * pi)
+  prob2 <- 0.5 + acos(((1 + Rho^2) * cos(Angles2) - 2 * Rho) / (1 + Rho^2 - 2 * Rho * cos(Angles2))) / (2 * pi)
+  c(prob1, prob2)
+}
+
+CDF_Card <- function(Rho, CircScale){
+  stopifnot(!(Rho == 0 | Rho > 0.5))
+  # if (Rho == 0 | Rho > 0.5) stop("Cardioid: 0 < Rho <= 0.5")
+  (CircScale + pi + 2 * Rho * sin(CircScale)) / (2 * pi)
+}
+
+r_Tri <- function(Rho, CumProbZ, N){
+  direction <- vector(mode = "numeric", length = N)
+
+  stopifnot(!(Rho == 0 | Rho > 4 / pi^2))
+  # if (Rho == 0 | Rho > 4 / pi^2) stop("Tri: 0 < Rho <= 4/pi^2")
+  filter <- CumProbZ < 0.5
+  u1 <- CumProbZ[filter]
+  a <- Rho / 8
+  b <- (4 + pi^2 * Rho) / (8 * pi)
+  c <- 0.5 - u1
+  q <- -.5 * (b + sqrt(b^2 - 4 * a * c))
+  direction[filter] <- c / q
+
+  u2 <- CumProbZ[!filter]
+  a <- -Rho / 8
+  b <- (4 + pi^2 * Rho) / (8 * pi)
+  c <- 0.5 - u2
+  q <- -.5 * (b + sqrt(b^2 - 4 * a * c))
+  direction[!filter] <- c / q
+  return(direction)
+}
+
+
 #' Simulate a circular random field
 #'
 #' @param N Number of spatial locations to simulate
@@ -86,13 +167,9 @@ SimulateCRF <- function(N = 100,
   # if (N <= 0 | Rho < 0 | Range < 0 | Ext <= 0 | Resolution <= 0) stop("Improper numeric input")
   stopifnot(!(N <= 0 | Rho < 0 | Range < 0 | Ext <= 0 | Resolution <= 0))
 
-  direction <- vector(mode = "numeric", length = N)
-
-  # require(CircStats)
-  # require(geoR)
   # Standard normal GRF, see Help geoR grf
   if (is.null(Grid)) {
-    GRF <- grf(
+    GRF <- geoR::grf(
       n = N,
       xlims = c(0, Range * Ext),
       ylims = c(0, Range * Ext),
@@ -104,7 +181,7 @@ SimulateCRF <- function(N = 100,
       messages = FALSE
     )
   } else {
-    GRF <- grf(
+    GRF <- geoR::grf(
       grid = Grid,
       cov.model = CovModel,
       nugget = 0,
@@ -128,71 +205,19 @@ SimulateCRF <- function(N = 100,
   if (CircDistr == "U") {
     direction <- -pi + 2 * pi * CumProbZ
   } else if (CircDistr == "Tri") {
-    stopifnot(!(Rho == 0 | Rho > 4 / pi^2))
-    # if (Rho == 0 | Rho > 4 / pi^2) stop("Tri: 0 < Rho <= 4/pi^2")
-    filter <- CumProbZ < 0.5
-    u1 <- CumProbZ[filter]
-    a <- Rho / 8
-    b <- (4 + pi^2 * Rho) / (8 * pi)
-    c <- 0.5 - u1
-    q <- -.5 * (b + sqrt(b^2 - 4 * a * c))
-    direction[filter] <- c / q
-
-    u2 <- CumProbZ[!filter]
-    a <- -Rho / 8
-    b <- (4 + pi^2 * Rho) / (8 * pi)
-    c <- 0.5 - u2
-    q <- -.5 * (b + sqrt(b^2 - 4 * a * c))
-    direction[!filter] <- c / q
+    direction <- r_Tri(Rho, CumProbZ, N)
   } else {
     # For OTHER circular distributions compute table of circular CDF and interpolate
     CircScale <- seq(-pi, pi, length = 2 * pi / Resolution)
     # With resolution=.01, circular support from -pi to +pi has 629 elements, delta ~0.01000507, CircScale[315] = 0
-    n <- length(CircScale)
     if (CircDistr == "vM") {
-      stopifnot(!(Rho == 0 | Rho >= 1))
-      # if (Rho == 0 | Rho >= 1) stop("vM: 0 < Rho < 1")
-      CircProb <- rep(-1, n)
-      Kappa <- CircStats::A1inv(Rho) # N. I Fisher, Statistical Analysis of Circular Data, 2000 p. 49
-      # As direction increases from -pi, pvm increases from .5
-
-      for (i in 1:length(CircScale)) CircProb[i] <- CircStats::pvm(CircScale[i], mu = 0, kappa = Kappa)
-      filter <- CircScale < 0
-      CircProb[filter] <- CircProb[filter] - 0.5
-      CircProb[!filter] <- CircProb[!filter] + 0.5
+      CircProb <- CDF_vM(Rho, CircScale)
     } else if (CircDistr == "Card") {
-      stopifnot(!(Rho == 0 | Rho > 0.5))
-      # if (Rho == 0 | Rho > 0.5) stop("Cardioid: 0 < Rho <= 0.5")
-      CircProb <- (CircScale + pi + 2 * Rho * sin(CircScale)) / (2 * pi)
+      CircProb <- CDF_Card(Rho, CircScale)
     } else if (CircDistr == "WrC") {
-      stopifnot(!(Rho == 0 | Rho >= 1))
-      # if (Rho == 0 | Rho >= 1) stop("Wrapped Cauchy: 0 < Rho < 1 ")
-      Angles1 <- CircScale[CircScale < 0]
-      Angles2 <- CircScale[CircScale >= 0]
-      prob1 <- 0.5 - acos(((1 + Rho^2) * cos(Angles1) - 2 * Rho) / (1 + Rho^2 - 2 * Rho * cos(Angles1))) / (2 * pi)
-      prob2 <- 0.5 + acos(((1 + Rho^2) * cos(Angles2) - 2 * Rho) / (1 + Rho^2 - 2 * Rho * cos(Angles2))) / (2 * pi)
-      CircProb <- c(prob1, prob2)
+      CircProb <- CDF_WrC(Rho, CircScale)
     }
-    CircProb[1] <- 0
-    CircProb[n] <- 1
-
-    # Interpolation
-    DeltaTh <- CircScale[2] + pi
-    for (i in 1:N)
-    {
-      p <- CumProbZ[i] # Cumulative prob of GRV
-      a <- max((1:n)[CircProb <= p]) # Index
-      if (a == n) {
-        r <- 0
-      } else {
-        if (CircProb[a] == p) {
-          r <- 0
-        } else {
-          r <- (p - CircProb[a]) / (CircProb[a + 1] - CircProb[a])
-        }
-      }
-      direction[i] <- CircScale[a] + r * DeltaTh
-    }
+    direction <- CDF2dir(CircProb, CumProbZ, N, CircScale)
   }
 
   direction <- direction + Mu
